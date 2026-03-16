f82369fac7a9148352aebda4258f6da1a667da0b
reyy maryblade <starshiiaman@gmail.com>
2026-03-16 19:57:12 +0600

    fix/ ui-ux

--- blog/views.py
+++ blog/views.py
@@ -7,7 +7,7 @@ from .forms import PostForm
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/blog.html', {'posts': posts})
+    return render(request, 'blog/post_list.html', {'posts': posts})
 
 
 def post_detail(request, pk):
